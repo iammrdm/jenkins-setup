2ad91d4bbb2f208a3d1df1905dc14a5707a98fe9
imrdm <imrmendez1112@gmail.com>
2024-08-27 14:48:35 +0800

    Remove data-sources.tf

--- assestment-infra-modular/data-sources.tf
+++
@@ -1,9 +0,0 @@
-data "terraform_remote_state" "assestment_infra_vpc" {
-  backend = "s3"
-
-  config = {
-    bucket = "oh-terraform"
-    key    = "assetment-infra/lm/assestment-infra-vpc.tfstate"
-    region = "ap-southeast-1"
-  }
-}
